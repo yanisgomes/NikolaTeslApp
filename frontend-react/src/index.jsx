3d115b84b415da24261df9ff0406a3da7401c64a
YanisGomes <yanis.gomes@ens-paris-saclay.fr>
2024-12-10 13:04:53 +0100

    dark mode correction and home update

--- frontend-react/src/index.jsx
+++ frontend-react/src/index.jsx
@@ -13,29 +13,44 @@ import ProfileContainer from './components/ProfileContainer/';
 
 import { ThemeProvider, SurveyProvider } from './utils/context/';
 
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh; // 100% de la hauteur de la fenêtre
+    padding-left: 30px;
+    padding-right: 30px;
+`;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
             <ThemeProvider>
                 <SurveyProvider>
-                    <GlobalStyle />
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="*" element={<Error />} />
-                        <Route
-                            path="/survey/:questionNumber"
-                            element={<Survey />}
-                        />
-                        <Route path="/results/" element={<Results />} />
-                        <Route path="/freelances/" element={<Freelances />} />
-                        <Route
-                            path="/profile/:id"
-                            element={<ProfileContainer />}
-                        />
-                    </Routes>
-                    <Footer />
+                    <MainContainer>
+                        <GlobalStyle />
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="*" element={<Error />} />
+                            <Route
+                                path="/survey/:questionNumber"
+                                element={<Survey />}
+                            />
+                            <Route path="/results/" element={<Results />} />
+                            <Route
+                                path="/freelances/"
+                                element={<Freelances />}
+                            />
+                            <Route
+                                path="/profile/:id"
+                                element={<ProfileContainer />}
+                            />
+                        </Routes>
+                        <Footer />
+                    </MainContainer>
                 </SurveyProvider>
             </ThemeProvider>
         </Router>
--- frontend-react/src/pages/Home/index.jsx
+++ frontend-react/src/pages/Home/index.jsx
@@ -1,23 +1,22 @@
-import imageNikola from '../../assets/NikolaTesla.png';
+import imgHome from '../../assets/fonction-transfert-home.png';
+import imgTesla from '../../assets/nikola-tesla-cartoon.png';
 import colors from '../../utils/style/colors.js';
 import styled from 'styled-components';
 
 const HorizontalContainer = styled.div`
     display: flex;
+    flex: 1 0 auto;
     flex-direction: row;
+
     justify-content: center;
     align-items: center;
     gap: 50px;
-    margin-top: 10px;
-    margin-bottom: 20px;
-    margin-left: 30px;
-    margin-right: 30px;
-    padding: 50px 50px;
     border-radius: 30px;
+
     background-color: ${colors.backgroundLight};
     transition: box-shadow 0.3s ease-in-out;
     &:hover {
-        box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.2);
+        box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
     }
 `;
 
@@ -25,12 +24,16 @@ const VerticalContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    align-items: center;
     height: 100%;
     width: 40%;
     background-color: ${colors.backgroundLight};
+    padding: 20px;
+    height: 80%;
 `;
 
-const StyledTitle = styled.h1`
+const StyledTitle = styled.h2`
+    color: ${colors.text};
     background-color: ${colors.backgroundLight};
 `;
 
@@ -38,20 +41,23 @@ const StyledButton = styled.button`
     width: 50%;
 `;
 
-const StyledImage = styled.img`
-    height: 10%;
+const StyledImageMain = styled.img`
+    width: 60vh;
+`;
+
+const StyledImageTesla = styled.img`
+    height: 50vh;
 `;
 
 function App() {
     return (
         <HorizontalContainer>
             <VerticalContainer>
-                <StyledTitle>
-                    Projet Nikola
-                </StyledTitle>
+                <StyledTitle>Déclenchez l'éclair de génie !</StyledTitle>
+                <StyledImageTesla src={imgTesla} alt="404" />
                 <StyledButton>Créer un nouveau circuit</StyledButton>
             </VerticalContainer>
-            <StyledImage src={imageNikola} alt="404" />
+            <StyledImageMain src={imgHome} alt="404" />
         </HorizontalContainer>
     );
 }
