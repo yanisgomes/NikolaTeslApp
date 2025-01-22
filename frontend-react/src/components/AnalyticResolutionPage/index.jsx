6a2d6c569176810d058b1f381ab29ee78b6b05bc
guillianaudry <guillian.audry@ens-paris-saclay.fr>
2025-01-22 23:01:40 +0100

    affichage des equations de resolution

--- frontend-react/src/components/AnalyticResolutionPage/index.jsx
+++ frontend-react/src/components/AnalyticResolutionPage/index.jsx
@@ -23,6 +23,31 @@ const AnalyticResolutionContainer = styled.div`
     padding: 0px;
 `;
 
+function CircuitDetails({ data }) {
+    return (
+        <div className="circuit-details">
+            {/* Fonction de transfert */}
+            <h2>Fonction de Transfert</h2>
+            <div className="transfer-function">
+                <LatexComponent
+                    latex={`\\text{H(s)} = ${data.transfer_function}`}
+                />
+            </div>
+
+            {/* Liste des explications et des équations */}
+            <h3>Explications et Équations</h3>
+            <ul className="explanations-list">
+                {data.explanations.map((explanation, index) => (
+                    <li key={index} className="explanation-item">
+                        <p>{explanation}</p>
+                        <LatexComponent latex={data.equations[index]} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
 const AnalyticResolutionPage = ({ onResolutionSubmit, ResolutionResponse }) => {
     const { circuitGraph, setCircuitGraph } = useContext(CircuitGraphContext);
     const { paper, setPaper } = useContext(PaperContext);
@@ -48,7 +73,7 @@ const AnalyticResolutionPage = ({ onResolutionSubmit, ResolutionResponse }) => {
                 <p>
                     Voici l’expression de la fonction de transfert du circuit :
                 </p>
-                <LatexComponent latex={ResolutionResponse.transfer_function} />
+                <CircuitDetails data={ResolutionResponse} />
             </div>
 
             <AnalyticComponentList />
