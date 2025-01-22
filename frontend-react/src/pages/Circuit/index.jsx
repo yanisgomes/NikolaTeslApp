d40c5a66dda0e3f5f0894f16bdc4e37c30671f03
YanisGomes <yanis.gomes@ens-paris-saclay.fr>
2025-01-22 23:02:30 +0100

    maya commit

--- frontend-react/src/pages/Circuit/index.jsx
+++ frontend-react/src/pages/Circuit/index.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useRef, useEffect, useContext } from 'react';
 import styled from 'styled-components';
 
-import Alert from '@mui/material/Alert';
-import { AlertTitle } from '@mui/material';
-
 import colors from '../../utils/style/colors';
 import fonts from './../../utils/style/fonts';
 
